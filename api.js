fc8a0180237ca01770564ff70e91aa47b43750f7
digvijay315 <143862901+digvijay315@users.noreply.github.com>
2026-02-07 13:02:10 +0530

    update

--- api.js
+++ api.js
@@ -1,15 +1,9 @@
 import axios from "axios";
 const instance = axios.create({
-<<<<<<< HEAD
-  baseURL: 'http://localhost:5002/'
-=======
 
+          // baseURL: 'http://localhost:5002/'
           baseURL:'https://newapi.bharatproperties.co/'
         // baseURL: 'http://localhost:5000/'
 
-
-
-
->>>>>>> 5e55ccce1cbd23ba6a2f7c2f5adc015699bdb853
 })
 export default instance;
