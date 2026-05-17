aa5d0c2c0a641a8a454a19f21dcd829b1c199d9c
Suraj keshwar <bharatproperties@Surajs-MacBook-Pro.local>
2026-05-17 17:32:30 +0530

    fix(settings): fix DiscoverySettingsPage api named import bug to resolve configs fetch failure

--- src/pages/Settings/views/DiscoverySettingsPage.jsx
+++ src/pages/Settings/views/DiscoverySettingsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from '../../../utils/api';
+import { api } from '../../../utils/api';
 import toast from 'react-hot-toast';
 
 const DiscoverySettingsPage = () => {
